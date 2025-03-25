99eb2a82fc8e5e05eddfa2817225a454c3cd637d
mannuccimattia <mannuccimattia@gmail.com>
2025-03-25 14:19:16 +0100

    [UPD] Done bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,28 @@
+const lampOff = document.getElementById("lamp-off");
+const lampOn = document.getElementById("lamp-on")
+const onOffBtn = document.getElementById("on-off-btn");
+
+onOffBtn.addEventListener("click", function(){
+  // se la lampadina è spenta
+  if(lampOff.classList.contains("shown") && lampOn.classList.contains("hidden")){
+    // tolgo l'immagine lamp-off
+    lampOff.classList.remove("shown");
+    lampOff.classList.add("hidden");
+    // inserisco l'immagine lamp-on
+    lampOn.classList.remove("hidden");
+    lampOn.classList.add("shown");
+    // cambio il testo del bottone da "Accendi" a "Spegni"
+    onOffBtn.innerHTML = "Spegni"
+  }
+  // se la lampadina è accesa
+  else if(lampOn.classList.contains("shown") && lampOff.classList.contains("hidden")){
+    // tolgo l'immagine lamp-on
+    lampOn.classList.remove("shown");
+    lampOn.classList.add("hidden");
+    // inserisco l'immagine lamp-off
+    lampOff.classList.remove("hidden");
+    lampOff.classList.add("shown");
+    // cambio il testo del bottone da "Spegni" a "Accendi"
+    onOffBtn.innerHTML = "Accendi"
+  }
+});
--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 const lampOff = document.getElementById("lamp-off");
-console.log(lampOff);
 const lampOn = document.getElementById("lamp-on")
-console.log(lampOn);
 const onOffBtn = document.getElementById("on-off-btn");
-console.log(onOffBtn);
 
 onOffBtn.addEventListener ('click', function(){
+  // tolgo l'immagine lamp-off
   lampOff.classList.remove("shown");
   lampOff.classList.add("hidden");
-
+  // inserisco l'immagine lamp-on
   lampOn.classList.remove("hidden")
   lampOn.classList.add("shown");
 });
